4e6ed1e49404c4216eed2717bcf5c8f997b8e792
Artem Radchenko <murano500k@gmail.com>
2021-12-07 22:42:04 +0200

    Added recent winner
    Signed-off-by: Artem Radchenko <murano500k@gmail.com>

--- front_end/src/components/LotteryState.tsx
+++ front_end/src/components/LotteryState.tsx
@@ -1,4 +1,4 @@
-import { useLotteryState, useLotteryBalance, useLotteryTimeStamp, useLotteryCounter } from "../hooks"
+import { useLotteryState, useLotteryBalance, useLotteryTimeStamp, useLotteryCounter, useRecentWinner } from "../hooks"
 import { makeStyles } from "@material-ui/core"
 
 
@@ -8,7 +8,12 @@ const useStyles = makeStyles((theme) => ({
         color: theme.palette.common.white,
         textAlign: "center",
         padding: theme.spacing(4)
-    }
+    },
+    subtitle: {
+        color: theme.palette.common.white,
+        textAlign: "center",
+        padding: theme.spacing(1)
+    },
 }))
 
 export const LotteryState = () => {
@@ -17,12 +22,14 @@ export const LotteryState = () => {
     const balance = useLotteryBalance()
     const counter = useLotteryCounter()
     const timestamp = useLotteryTimeStamp()
+    const recentWinner = useRecentWinner()
     return (
         <>
-            <h2 className={classes.title}> Lottery is {lotteryState} now</h2>
-            <h2 className={classes.title}> Lottery balance is {balance} ethers</h2>
-            <h2 className={classes.title}> Lottery counter is {counter ? counter.toNumber() : -10}</h2>
-            <h2 className={classes.title}> last updated {timestamp}</h2>
+            <h2 className={classes.subtitle}> Lottery is {lotteryState} now</h2>
+            <h2 className={classes.subtitle}> Lottery balance is {balance} ethers</h2>
+            <h2 className={classes.subtitle}> Recent winner is {recentWinner}</h2>
+            <h2 className={classes.subtitle}> Lottery counter is {counter ? counter.toNumber() : -10}</h2>
+            <h2 className={classes.subtitle}> last updated {timestamp}</h2>
 
         </>)
 }
